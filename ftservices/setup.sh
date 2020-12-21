940595d243d165b138b1d9a13d0cc15ff8120f91
user42 <ted.bajrami@gmail.com>
2020-12-21 23:25:09 +0100

    r

--- ftservices/setup.sh
+++ ftservices/setup.sh
@@ -5,15 +5,16 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl create -f config.yaml
+kubectl apply -f config.yaml
 
 #INSTALL NGINX
 eval $(minikube docker-env)
 docker build -t s-nginx ./nginx
-kubectl create -f nginx.yaml
+kubectl apply -f nginx.yaml
 
 #RUNNING DASHBOARD
 #minikube dashboard &
