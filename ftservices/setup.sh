3cfcc09be21e2b1f1cbd566f4a447bb642f8baef
user42 <ted.bajrami@gmail.com>
2020-12-26 03:06:40 +0100

    not working

--- ftservices/setup.sh
+++ ftservices/setup.sh
@@ -19,17 +19,11 @@ docker build -t s-phpmyadmin ./srcs/phpmyadmin
 docker build -t s-grafana ./srcs/grafana
 docker build -t s-influxdb ./srcs/influxdb
 
-kubectl create namespace monitoring
 kubectl create secret generic influxdb-creds \
-  --from-literal=INFLUXDB_DB=monitoring \
-  --from-literal=INFLUXDB_USER=user \
-  --from-literal=INFLUXDB_USER_PASSWORD=<password> \
-  --from-literal=INFLUXDB_READ_USER=readonly \
-  --from-literal=INFLUXDB_USER_PASSWORD=<password> \
-  --from-literal=INFLUXDB_ADMIN_USER=root \
-  --from-literal=INFLUXDB_ADMIN_USER_PASSWORD=<password> \
-  --from-literal=INFLUXDB_HOST=influxdb  \
-  --from-literal=INFLUXDB_HTTP_AUTH_ENABLED=true
+  --from-literal=INFLUXDB_DATABASE=database \
+  --from-literal=INFLUXDB_USERNAME=user \
+  --from-literal=INFLUXDB_PASSWORD=password \
+  --from-literal=INFLUXDB_HOST=influxdb
 kubectl apply -f telegraf.yaml
 
 #DEPLOY SERVICES
