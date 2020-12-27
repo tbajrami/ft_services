f7eea6c1fc17ceb9e9d538787d0e699410890733
user42 <ted.bajrami@gmail.com>
2020-12-27 13:46:58 +0100

    fini !

--- ftservices/srcs/ftps/start.sh
+++ ftservices/srcs/ftps/start.sh
@@ -1,5 +1,11 @@
 mkdir -p /var/ftp
 
-adduser -D -h /var/ftp user42
-telegraf &
-vsftpd /etc/vsftpd/vsftpd.conf
+mkdir -p srcs/ftp/user
+echo -e "password\npassword" | adduser -h /srcs/ftp/user user
+chown user:user /srcs/ftp/user
+mkdir /srcs/ftp/user/folder_text
+chown user:user /srcs/ftp/user/folder_text
+touch /srcs/ftp/user/folder_text/text.txt
+
+#telegraf &
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf & sleep infinite
